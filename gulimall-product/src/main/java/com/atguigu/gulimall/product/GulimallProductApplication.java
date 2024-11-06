3c40b240edbe6702daadbf67829b29a137b1002e
QingJiu <2056316661@qq.com>
2024-11-07 00:37:53 +0800

    项目初始结构的品牌文件内容的JSR303校验

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+/**
+ * JSR303:
+ *  给需要的校验的数据加上注解，给Bean添加注解：javax.validation.constraints，并定义自己的message提示
+ *  只标注校验规则，不在控制层，开启校验规则也是不起作用 ;开启校验@Valid:
+ * 	    效果：校验错误会有默认的响应：
+ *  给校验的bean后紧跟一个BindingResult,就可以获取到校验结果
+ */
 @MapperScan("com.atguigu.gulimall.product.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -6,6 +6,10 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
@@ -25,12 +29,19 @@ public class BrandEntity implements Serializable {
 	@TableId
 	private Long brandId;
 	/**
-	 * 品牌名
+	 * 品牌名 不能为空
+	 * @NotNull、@NotEmpty和@NotBlank
+	 * @NotEmpty:表示所标注的元素必须不是null或者空串
+	 * @NotBlank:表示所标注的元素，必须包含至少有一个非空字符（不能是空格，至少有一个非空格字符）
+	 * 这些注解除了可以对其进行校验判断外，还以自定义其校验内容;
 	 */
+	@NotBlank(message = "品牌名必须提交")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotEmpty
+	@URL(message = "logo必须是一个合法url地址")
 	private String logo;
 	/**
 	 * 介绍
@@ -39,14 +50,20 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+
 	private Integer showStatus;
 	/**
-	 * 检索首字母
+	 * 检索首字母 JSR303并未提供，但是可以通过@Pattern往里传入正则表达式实现
 	 */
+	@NotEmpty
+	@Pattern(regexp = "/^[a-zA-Z]]$/",message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
-	 * 排序
+	 * 排序：
+	 *  注意：@NotEmpty不能为整数字段表示空需要使用@NotNull
 	 */
+	@NotNull
+	@Min(value = 0,message = "排序必须大于等于0")
 	private Integer sort;
 
 }
